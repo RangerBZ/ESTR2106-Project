ed23b3139b7b3b516f30404d134eddf929685473
Ray_Amidst <bingzhe28@hotmail.com>
2024-12-09 15:42:13 +0800

    add the styles example

--- client/src/components/Events.js
+++ client/src/components/Events.js
@@ -1,8 +1,14 @@
 import React  from 'react';
+import './../styles/Events.css';
 import { useParams } from 'react-router-dom';
 
 class Events extends React.Component {
-
+    render(){
+        return(<div className='Events'>
+            
+        </div>
+        );
+    }
 }
 
 export default Events;
