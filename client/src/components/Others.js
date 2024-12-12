9456ca1e5c091774607f4156c778d6a687d12440
Ray_Amidst <bingzhe28@hotmail.com>
2024-12-12 09:15:16 +0800

    push the new test

--- client/src/components/Others.js
+++ client/src/components/Others.js
@@ -5,7 +5,7 @@ import { useParams } from 'react-router-dom';
 class Others extends React.Component {
     render(){
         return(<div className='Others'>
-            
+            Others
         </div>
         );
     }
