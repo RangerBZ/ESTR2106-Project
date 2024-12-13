b03356350bf6488723439fbcc5e3ce515efe62a4
cuhk11049 <150638858+cuhk11049@users.noreply.github.com>
2024-12-14 00:02:10 +0800

    Add files via upload

--- client/src/components/Manage.js
+++ client/src/components/Manage.js
@@ -1,10 +1,92 @@
 import React from 'react';
-import {BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 class Manage extends React.Component {
-    render(){
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentPage: 'NewEvent',
+        };
+    }
+
+    navigateTo = (page) => {
+        this.setState({ currentPage: page });
+    };
+
+    render() {
+        const routerStyle = {
+            listStyle: 'none',
+            padding: 0,
+            margin: 0,
+            display: 'flex',
+            justifyContent: 'flex-start',
+            gap: '20px',
+        };
+
+        const linkStyle = {
+            textDecoration: 'none',
+            color: 'black',
+            cursor: 'pointer',
+        };
+
+        const { currentPage } = this.state;
+
         return (
-           <div></div>
+            <div>
+                {/* 导航栏 */}
+                <ul style={routerStyle}>
+                    <li style={linkStyle} onClick={() => this.navigateTo('NewEvent')}>New Event</li>
+                    <li style={linkStyle} onClick={() => this.navigateTo('ModifyEvent')}>Modify Event</li>
+                    <li style={linkStyle} onClick={() => this.navigateTo('CreateUser')}>Create User</li>
+                    <li style={linkStyle} onClick={() => this.navigateTo('ModifyUser')}>Modify User</li>
+                </ul>
+
+                <div>
+                    {currentPage === 'NewEvent' && <NewEvent />}
+                    {currentPage === 'ModifyEvent' && <ModifyEvent />}
+                    {currentPage === 'CreateUser' && <CreateUser />}
+                    {currentPage === 'ModifyUser' && <ModifyUser />}
+                </div>
+            </div>
+        );
+    }
+}
+
+class NewEvent extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>NewEvent</p>
+            </div>
+        );
+    }
+}
+
+class ModifyEvent extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>ModifyEvent</p>
+            </div>
+        );
+    }
+}
+
+class CreateUser extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>CreateUser</p>
+            </div>
+        );
+    }
+}
+
+class ModifyUser extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>ModifyUser</p>
+            </div>
         );
     }
 }
